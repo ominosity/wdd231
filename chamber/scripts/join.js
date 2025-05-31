f0a46831142d786d8ac30b067f3713ac57eed8fa
ominosity <ominosity@msn.com>
2025-05-31 13:28:09 -0700

    Added animations on membership card dialogs. Added closing X's to dialogs, since the closedby="auto" is experimental and doesn't validate.

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -3,10 +3,16 @@ const npCardElement = document.querySelector('#np-card');
 const bronzeCardElement = document.querySelector('#bronze-card');
 const silverCardElement = document.querySelector('#silver-card');
 const goldCardElement = document.querySelector('#gold-card');
+
 const npDialog = document.querySelector('#np-dialog');
 const bronzeDialog = document.querySelector('#bronze-dialog');
 const silverDialog = document.querySelector('#silver-dialog');
 const goldDialog = document.querySelector('#gold-dialog');
+
+const npDialogClose = document.querySelector('#np-dialog div.close');
+const bronzeDialogClose = document.querySelector('#bronze-dialog div.close');
+const silverDialogClose = document.querySelector('#silver-dialog div.close');
+const goldDialogClose = document.querySelector('#gold-dialog div.close');
 
 const timestampElement = document.querySelector('#timestamp');
 
@@ -16,4 +22,11 @@ bronzeCardElement.addEventListener('click', () => bronzeDialog.showModal());
 silverCardElement.addEventListener('click', () => silverDialog.showModal());
 goldCardElement.addEventListener('click', () => goldDialog.showModal());
 
+/* Close the modal when the relevant x-element is clicked */
+npDialogClose.addEventListener('click', () => npDialog.close());
+bronzeDialogClose.addEventListener('click', () => bronzeDialog.close());
+silverDialogClose.addEventListener('click', () => silverDialog.close());
+goldDialogClose.addEventListener('click', () => goldDialog.close());
+
+/* Add the current timestamp to the hidden input (for display on thank you page) */
 timestampElement.setAttribute('value', new Date());
